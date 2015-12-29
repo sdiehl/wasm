e6e9ddd112d3f9c2db4f98d4cc1fc31f1b95923d
Stephen Diehl <stephen.m.diehl@gmail.com>
2015-12-29 16:00:10 -0500

    entry module

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,30 +1,6 @@
 module Main where
 
-import Lexer
-import Parser
-import Monad
-import Syntax
-import Pretty
-import Eval
-import Verify
-
-import Control.Monad
-import Control.Applicative
-
-parse :: String -> Either ParseError [Decl]
-parse fs = runParseM prog (scan fs)
-
-file :: FilePath -> IO (Either ParseError [Decl])
-file fname = do
-  contents <- readFile fname
-  {-print $ scan contents-}
-  return $ parse contents
+import qualified Entry
 
 main :: IO ()
-main = do
-  ast1 <- file "example1.wasm"
-  ast2 <- file "example2.wasm"
-  print ast1
-  print ast2
-  putStrLn "Done"
-  return ()
+main = Entry.main
