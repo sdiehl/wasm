6d4fcbdaacf2cf077682c91897be774fdeaa0060
Stephen Diehl <stephen.m.diehl@gmail.com>
2016-01-07 20:50:27 -0500

    abstract out logic

--- src/Binary.hs
+++ src/Binary.hs
@@ -1,11 +1,12 @@
-{-# OPTIONS_GHC -fdefer-type-errors #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module Binary where
 
 import Data.Word
+import Data.Word
 import Data.ByteString
 import Data.Serialize
+import Data.Serialize.Put
 import qualified Data.List as List
 
 import Syntax
@@ -23,6 +24,21 @@ data WasmSectionType
   | SectionFunctionTable
   | SectionEnd
   deriving (Eq, Show)
+
+data WasmFunctionFlag
+  = FunctionFlagName
+  | FunctionFlagImport
+  | FunctionFlagLocals
+  | FunctionFlagExport
+  deriving (Eq, Show)
+
+instance Serialize WasmFunctionFlag where
+  put x = case x of
+    FunctionFlagName   -> putWord8 1
+    FunctionFlagImport -> putWord8 2
+    FunctionFlagLocals -> putWord8 4
+    FunctionFlagExport -> putWord8 8
+  get = error "get Type"
 
 instance Serialize WasmSectionType where
   put x = case x of
@@ -54,20 +70,54 @@ instance Serialize Decl where
 
 instance Serialize Func where
   put (Export name _) = do
-    putWord32le 15
-    put name
+    putWord8 0x74
+    putWord8 0x65
+    putWord8 0x73
+    putWord8 0x74
+
   put (Func ftype fparams body) = do
-    putWord32le 0
-    putWord32le 0
-    putWord8 40
     putWord8 09
-    putWord8 02
-    putWord32le 05
+    putWord16le 0x0000        -- function signature index
+    putWord32le 0x00000015    -- function name offset
+    putWord8 0x05             -- function body size
+    putWord8 0x00
+    mapM_ put fparams
+    put SectionEnd
+
+  put (Import name _) = do
+    undefined
+  get = error "get Type"
+
+instance Serialize Param where
+  put (Body x) = put x
+  put (Result x) = return ()
   get = error "get Type"
 
 putOp :: (Binop, Type) -> Put
 putOp (op, I32) = case op of
-  Add -> putWord8 0x40
+  Add      -> putWord8 0x40
+  Sub      -> todo
+  Mul      -> todo
+  DivS     -> todo
+  DivU     -> todo
+  RemS     -> todo
+  RemU     -> todo
+  And      -> todo
+  Or       -> todo
+  Xor      -> todo
+  Shl      -> todo
+  ShrU     -> todo
+  ShrS     -> todo
+  Div      -> todo
+  CopySign -> todo
+  Min      -> todo
+  Max      -> todo
+
+instance Serialize Value where
+  put x = case x of
+    VInt y   -> putWord8 (fromIntegral y)
+    VFloat y -> todo
+  get = error "get Type"
 
 instance Serialize Expr where
   put x = case x of
@@ -82,7 +132,12 @@ instance Serialize Expr where
     Br y1 y2           -> todo
     Return y           -> todo
     Call y1 y2         -> todo
-    Const y1 y2        -> todo
+    Const ty val       -> do
+      case ty of
+        I32 -> do
+          putWord8 0x09
+          put val
+
     Lit y              -> todo
     Load y1 y2         -> todo
     Store y1 y2        -> todo
@@ -95,10 +150,12 @@ instance Serialize Expr where
     Sel y1 y2 y3 y4    -> todo
     Convert y1 y2      -> todo
     Host y1 y2         -> todo
-    Bin op ty x1 x2    -> do
+
+    Bin op ty x1 x2 -> do
       putOp (op, ty)
       put x1
       put x2
+
   get = error "get Type"
 
 instance Serialize Module where
@@ -114,22 +171,24 @@ instance Serialize Module where
     put SectionFunction
     putWord8 1                -- num functions
 
-    -- Function[0]
-    putWord16le 0x0009        -- function decl
-    putWord32le 0x00001500
-    putWord16le 0x0500
+    mapM_ put funs
+    {-mapM_ put [(Bin Add I32 (Const I32 (VInt 1)) (Const I32 (VInt 2)))]-}
 
-    putWord16le 0x4000 -- ExprI32Add
-    putWord16le 0x0109 -- ExprI8Const(1)
-    putWord16le 0x0209 -- ExprI8Const(2)
-    put SectionEnd
-    {-putWord8 0x6       -- WasmSectionEnd-}
+    -- Function[0]
+    {-putWord8 09-}
+    {-putWord16le 0x0000        -- function signature index-}
+    {-putWord32le 0x00000015    -- function name offset-}
+    {-putWord8 0x05             -- function body size-}
+    {-putWord8 0x00-}
+
+    {-put (Bin Add I32 (Const I32 (VInt 1)) (Const I32 (VInt 2)))-}
+    {-put SectionEnd-}
 
     -- export name
-    putWord8 0x74
-    putWord8 0x65
-    putWord8 0x73
-    putWord8 0x74
+    {-putWord8 0x74-}
+    {-putWord8 0x65-}
+    {-putWord8 0x73-}
+    {-putWord8 0x74-}
   get = error "get Module"
 
 todo :: t
