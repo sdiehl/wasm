55df6839632b0c8e0302ba20f2e2586131231ac1
faineance <faineance@users.noreply.github.com>
2017-04-06 17:41:19 +0100

    init

--- src/Language/Wasm/Validator.hs
+++ src/Language/Wasm/Validator.hs
@@ -1,1 +1,15 @@
 module Language.Wasm.Validator where
+import Control.Applicative.Lift
+import Language.Wasm.Core
+
+data Mutability = Immutable | Mutable deriving (Eq, Show)
+
+data GlobalType = GlobalType Type Mutability deriving (Eq, Show)
+
+data Context = Context
+  { mod     :: Module
+  , globals :: [GlobalType]
+  , locals  :: [Type]
+  , results :: [Type]
+  , labels  :: [[Type]]
+  } deriving (Eq, Show)
